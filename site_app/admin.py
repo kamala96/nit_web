456e69b8ea70c3c9a672904a8d363de8f57ab64b
kamala96 <jovinjohn959@gmail.com>
2024-03-13 13:51:57 +0300

    Added blueprint for News Segment

--- site_app/admin.py
+++ site_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from site_app.models import Menu
+from site_app.models import Category, Event, Menu, Post
 
 # Register your models here.
 
@@ -12,3 +12,29 @@ class MenuAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_filter = ['parent_menu']
     search_fields = ['title', 'slug',]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'description', 'created_at', 'updated_at']
+    list_per_page = 10
+    list_filter = ['name']
+    search_fields = ['name',]
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description',  'address',
+                    'image_url', 'start_date', 'end_date', 'status', 'created_at', 'updated_at']
+    list_per_page = 10
+    list_filter = ['title',]
+    search_fields = ['title', 'address',]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'description',
+                    'file_url', 'image_url', 'web_url', 'user', 'created_at', 'updated_at']
+    list_per_page = 10
+    list_filter = ['title', 'category']
+    search_fields = ['title',]
--- site_app/models.py
+++ site_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -15,3 +16,52 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name.upper()
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    address = models.CharField(
+        max_length=200, verbose_name="Address/Location/Venue")
+    image_url = models.ImageField(upload_to='uploads/events/', null=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.BooleanField(
+        default=False, help_text='Whether it is publishable or not (draft)')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title.upper()
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    file_url = models.FileField(
+        upload_to='uploads/files/', null=True, blank=True)
+    image_url = models.ImageField(
+        upload_to='uploads/images/', null=True, blank=True)
+    web_url = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title.upper()
