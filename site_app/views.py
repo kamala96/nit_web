cab55aeceba7ac2c1acd2ae77c72b9a43b4c3033
kamala96 <jovinjohn959@gmail.com>
2024-03-14 09:12:16 +0300

    Modified Menue Model

--- site_app/views.py
+++ site_app/views.py
@@ -7,17 +7,17 @@ from site_app.models import Menu
 
 def index(request):
     top_menus = Menu.objects.filter(parent_menu__isnull=True)
-    chunk_len = 4
-    
-    menu_data = []
-    for top_menu in top_menus:
-        submenus = top_menu.submenus.all()
-        # Sort by submenu_head, treating None as an empty string
-        submenus = sorted(submenus, key=lambda x: x.submenu_head or '')
-        submenu_chunks = [list(group) for _, group in groupby(submenus, key=lambda x: x.submenu_head)]
-        menu_data.append({'top_menu': top_menu, 'submenu_chunks': submenu_chunks})
-    
+    # chunk_len = 4
+
+    # menu_data = []
+    # for top_menu in top_menus:
+    #     submenus = top_menu.submenus.all()
+    #     # Sort by submenu_head, treating None as an empty string
+    #     submenus = sorted(submenus, key=lambda x: x.submenu_head or '')
+    #     submenu_chunks = [list(group) for _, group in groupby(submenus, key=lambda x: x.submenu_head)]
+    #     menu_data.append({'top_menu': top_menu, 'submenu_chunks': submenu_chunks})
+
     context = {
-        'menus': menu_data,
+        'menus': top_menus,
     }
     return render(request, 'index.html', context)
