f5e1306d75f4468e8677d43c49f7e93dfa51ccee
kamala96 <jovinjohn959@gmail.com>
2024-03-16 11:58:46 +0300

    Sync with George

--- site_app/context_processors.py
+++ site_app/context_processors.py
@@ -5,5 +5,5 @@ from .models import Menu
 
 def menu_context(request):
     # Retrieve all menu items
-    top_menus = Menu.objects.filter(parent_menu__isnull=True)
+    top_menus = Menu.objects.filter(parent_menu__isnull=True, is_visible=True)
     return {'menus': top_menus}
